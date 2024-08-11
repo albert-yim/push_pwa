1c9edeb0e8ad46eca41b06fea5abf018c6c6bc3f
albert.yim <albert.yim719@gmail.com>
2024-08-11 19:26:31 +0900

    Add logic to request notification permission

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { getToken } from "firebase/messaging";
+import { messaging } from "./firebase";
 
 function App() {
+  const [token, setToken] = useState("");
+  const [isClicked, setIsClicked] = useState(false);
+  useEffect(() => {
+    async function requestPermission() {
+      const isSupported = () =>
+        "Notification" in window &&
+        "serviceWorker" in navigator &&
+        "PushManager" in window;
+      if (!isSupported) return;
+      const permission = await Notification.requestPermission();
+
+      if (permission === "granted") {
+        console.log("get permission");
+        try {
+          const currentToken = await getToken(messaging, {
+            vapidKey: process.env.REACT_APP_VAPID_KEY,
+          });
+          console.log("currentToken", currentToken);
+          setToken(currentToken);
+        } catch (error) {
+          console.log("Error getting token", error);
+        }
+      } else {
+        console.log("denided permission");
+      }
+    }
+    requestPermission();
+  }, [isClicked]);
+  const clicked = () => {
+    setIsClicked(!isClicked);
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <s>
+          support? {isSupported ? "yes" : "no"} token: {token}
+        </s>
+        <button onClick={clicked}>Request Permission</button>
       </header>
     </div>
   );
