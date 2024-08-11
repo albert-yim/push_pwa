e1eae1ce7b5e0c1d495abfe8d5fa10fab3f6f50e
albert.yim <albert.yim719@gmail.com>
2024-08-11 11:55:37 +0900

    reorganize firebase files and private key

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "../config/firebaseConfig";
+import { getMessaging } from "firebase/messaging";
+
+//Firebase Config
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+//init firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Firebase Cloud Messaging and get a reference to the service
+const messaging = getMessaging(app);
+
+export { messaging };
